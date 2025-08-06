1d123d6be207e88a3bf87e87bde106f66a5badf0
zeshanalvi <zeshankhanalvi@gmail.com>
2025-08-06 21:19:57 +0500

    Update nn_architecture.py

--- nn_architecture.py
+++ nn_architecture.py
@@ -1,22 +1,30 @@
 from tensorflow.python.platform import tf_logging as logging
 
+
+import logging
+from tensorflow.keras.callbacks import ReduceLROnPlateau
+
 class ReduceLRBacktrack(ReduceLROnPlateau):
-  def __init__(self, best_path, *args, **kwargs):
-    super(ReduceLRBacktrack, self).__init__(*args, **kwargs)
-    self.best_path = best_path
+    def __init__(self, best_path, *args, **kwargs):
+        super(ReduceLRBacktrack, self).__init__(*args, **kwargs)
+        self.best_path = best_path
 
-  def on_epoch_end(self, epoch, logs=None):
-    current = logs.get(self.monitor)
-    if current is None:
-        logging.warning('Reduce LR on plateau conditioned on metric `%s` which is not available. Available metrics are: %s',self.monitor, ','.join(list(logs.keys())))
-    if not self.monitor_op(current, self.best): # not new best
-        if not self.in_cooldown(): # and we're not in cooldown
-            if self.wait+1 >= self.patience: # going to reduce lr
-                # load best model so far
-                print("Backtracking to best model before reducting LR")
-                self.model.load_weights(self.best_path)
+    def on_epoch_end(self, epoch, logs=None):
+        current = logs.get(self.monitor)
+        if current is None:
+            logging.warning(
+                'Reduce LR on plateau conditioned on metric `%s` which is not available. Available metrics are: %s',
+                self.monitor, ','.join(list(logs.keys()))
+            )
 
-    super().on_epoch_end(epoch, logs) # actually reduce LR
+        if not self.monitor_op(current, self.best):  # not a new best
+            if not self.in_cooldown():               # and we're not in cooldown
+                if self.wait + 1 >= self.patience:   # going to reduce lr
+                    print("Backtracking to best model before reducing LR")
+                    self.model.load_weights(self.best_path)
+
+        super().on_epoch_end(epoch, logs)  # actually reduce LR
+
 
 class TreeNet:
   def __init__(self,selected_model_name,output_layer,weights,dataset,epochs,test_size,trained_weights_path):
