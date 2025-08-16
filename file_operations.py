58d3fce65f33fd050c746906772a01b46031567a
zeshanalvi <zeshankhanalvi@gmail.com>
2025-08-16 08:35:51 +0500

    Rectification

--- file_operations.py
+++ file_operations.py
@@ -1,146 +1,59 @@
-from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
-import numpy as np, random, os, math, cv2, os, h5py, pandas as pd
+import os, pandas as pd
+def subset(trainX,trainY,chunk=1):
+  shuffled_indices = np.random.permutation(len(trainX))
+  # Apply the same shuffle to both
+  trainX = trainX.iloc[shuffled_indices].reset_index(drop=True)
+  trainY = trainY[shuffled_indices]
+  # Select 50% of the rows
+  half_size = int(len(trainX)*chunk)
+  trainX = trainX.iloc[:half_size]
+  trainY = trainY[:half_size]
 
-
-class Dataset:
-  def __init__(self, label_map,num_classes, images_base_path):
-    self.label_map = label_map
-    self.num_classes=num_classes
-    self.images_base_path=images_base_path
-    self.img_rows=224
-    self.img_cols=224
-    self.channels=3
-    self.file_names=[]
-    self.images=[]
-    self.label=[]
-    self.labels=[]
-
-  def gather_paths_all(self):
-    i=0
-    jpg_path=self.images_base_path
-    folder=os.listdir(jpg_path)
-    count=0
-    if (os.path.isfile(jpg_path+folder[0])): # Single Directory for all files
-      count=len(os.listdir(jpg_path))
+def read_files(base_path):
+  #base_path="/kaggle/input/kvasirv1/"
+  files=[base_path+f for f in os.listdir(base_path) if ".csv" in f and "dev_" in f and "_500_" not in f and "Haralick" not in f and "lbp." not in f]# and "deep_" not in f]
+  files.sort()
+  #print(len(files),files)
+  trainX=None
+  testX=None
+  trainY=None
+  testY=None
+  for f in files:
+    #print(f.split("/")[-1])
+    df=pd.read_csv(f)
+    df['filename']=df['class1']+"_"+df['img']
+    #df['filename']=df['labels']+"_"+df['filename']
+    df=df.sort_values(by='filename')
+    if(trainX is None):
+      trainX=df.drop(columns={'class','img','class1','filename'},axis=1)
+      trainY=df["class"]-1
+      trainY, _ = pd.factorize(trainY)
     else:
-      count=sum([len(os.listdir(jpg_path+f)) for f in os.listdir(jpg_path)]) # Class wise directory
-    ima=['' for x in range(count)] #Blank Image Path Array
-    labels=np.zeros((count,num_classes),dtype=float) # Label Vectors
-    label=[0 for x in range(count)] # Label Numbers
-    if (os.path.isfile(jpg_path+folder[0])):
-      for f in folder:
-        im=jpg_path+f
-        ima[i]=im
-        label[i]=0
-        i+=1
-        if(count<i):
-          break # All images processed
+      trainX=trainX.merge(df.drop(columns={'class','img','class1','filename'},axis=1),left_index=True,right_index=True,how='inner',suffixes=("","_"+f.split('/')[-1].split(".")[0][4:9]))
+      
+    #print(df.shape,len(train_X),train_X[0].shape)
+
+  for f in files:
+    f=f.replace("dev_","val_")
+    #print(f)
+    df=pd.read_csv(f)
+    df['filename']=df['class1']+"_"+df['img']
+    #df['filename']=df['labels']+"_"+df['filename']
+    df=df.sort_values(by='filename')
+    if(testX is None):
+      testX=df.drop(columns={'class','img','class1','filename'},axis=1)
+      testY=df["class"]-1
+      testY, _ = pd.factorize(testY)
     else:
-      for fldr in folder:
-        for f in os.listdir(jpg_path+fldr+"/"):
-            im=jpg_path+fldr+"/"+f
-            ima[i]=im
-            label[i]=self.label_map.index(fldr)+1
-            i+=1
-        if(count<=i):
-            break # All images processed
-    for i in range(count):
-        labels[i][label[i]-1]=1 # Labels to Label Vectors
-    return ima, label, labels
-  def fractional(self,fraction=0.5):
-    #shuffle
-    frac=int(fraction*len(self.images))
-    print("selected ",frac," images from ",len(self.images))
-    combined = list(zip(self.images, self.label))
-    random.shuffle(combined)
-    selected = combined[:frac]
-    ima_selected, label_selected = zip(*selected)
-    ima = list(ima_selected)
-    label = list(label_selected)    
-    labels=np.zeros((frac,self.num_classes),dtype=float)    
-    for i in range(frac):
-        labels[i][label[i]-1]=1
-    return ima, label, labels
+      testX=testX.merge(df.drop(columns={'class','img','class1','filename'},axis=1),left_index=True,right_index=True,how='inner',suffixes=("","_"+f.split('/')[-1].split(".")[0][4:9]))
+    #print(f.split("/")[-1],df.shape,len(test_X),test_X[0].shape)
+  df=None
+  return trainX,trainY,testX,testY
 
-  def gather_paths_some(self,fraction=0.5):
-    jpg_path=self.images_base_path
-    print("Extracting Fraction of \t",fraction)
-    i=0
-    folder=os.listdir(jpg_path)
-    count=0
-    if (os.path.isfile(jpg_path+folder[0])):
-        count=len(os.listdir(jpg_path))
-    else:
-        count=sum([int(len(os.listdir(jpg_path+f))) for f in os.listdir(jpg_path)])
-    ima=['' for x in range(count)]
-    label=[0 for x in range(count)]
-    if (os.path.isfile(jpg_path+folder[0])):
-        for f in folder:
-            im=jpg_path+f
-            ima[i]=im
-            label[i]=0
-            i+=1
-            if(count<i):
-                break
-    else:
-        for fldr in folder:
-            br=0
-            for f in os.listdir(jpg_path+fldr+"/"):
-                im=jpg_path+fldr+"/"+f
-                ima[i]=im
-                label[i]=self.label_map.index(fldr)+1
-                i+=1
-                br+=1
-                #print(br,i,math.ceil(len(os.listdir(jpg_path+fldr+"/"))/2))
-                if(br>=math.ceil(len(os.listdir(jpg_path+fldr+"/")))):
-                    break
-                if(count<=i):
-                    break
-            if(count<=i):
-                break
-    
-    #shuffle
-    frac=int(fraction*len(ima))
-    print("selected ",frac," images from ",len(ima))
-    combined = list(zip(ima, label))
-    random.shuffle(combined)
-    selected = combined[:frac]
-    ima_selected, label_selected = zip(*selected)
-    ima = list(ima_selected)
-    label = list(label_selected)    
-    labels=np.zeros((frac,self.num_classes),dtype=float)    
-    for i in range(frac):
-        labels[i][label[i]-1]=1
-    return ima, label, labels
 
-  def gather_images_from_paths(self,start,count):
-    paths_all=self.file_names
-    print('Stats of Images Start:',start,' To:',(start+count),'All Images:',len(paths_all))
-    ima=np.zeros((count,self.img_rows,self.img_cols,self.channels),np.uint8)
-    for i in range(count):
-        #print(i,count,paths_all[start+i])
-        img=cv2.imread(paths_all[start+i])
-        im = cv2.resize(img, (self.img_rows, self.img_cols)).astype(np.uint8)
-        ima[i]=im
-    return ima
 
-  def get_images(self):
-    paths=self.file_names
-    count=len(paths)
-    ima=np.zeros((count,self.img_rows,self.img_cols,self.channels),np.uint8)
-    for i in range(count):
-        img=cv2.imread(paths[i])
-        im = cv2.resize(img, (self.img_rows, self.img_cols)).astype(np.uint8)
-        ima[i]=im
-    return ima
 
-  def gather_images_from_paths_HSV(self,start,count):
-    jpg_path=self.images_base_path
-    print('Stats of Images Start:',start,' To:',(start+count),'All Images:',len(jpg_path))
-    ima=np.zeros((count,self.img_rows,self.img_cols,self.channels),np.uint8)
-    for i in range(count):
-      img=cv2.imread(jpg_path[start+i])
-      img_rgb = cv2.resize(img, (self.img_rows, self.img_cols)).astype(np.uint8)
-      img_hsv = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2HSV)
-      ima[i]=img_hsv
-    return ima
+
+
+
+read_files(base_path="/kaggle/input/kvasirv1/")
--- nn_architecture.py
+++ nn_architecture.py
@@ -1,6 +1,3 @@
-import numpy as np
-import pandas as pd
-import os
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix,f1_score, matthews_corrcoef, precision_score, recall_score
 #classifiers
@@ -9,195 +6,73 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier,
 #!pip install xgboost
 #!pip install lightgbm
 #!pip install catboost
-
+import numpy as np, pandas as pd
 from xgboost import XGBClassifier
-import lightgbm as lgb
+#import lightgbm as lgb
 from catboost import CatBoostClassifier
 import random
-
-
-base_path="/kaggle/input/kvasirv1/"
-class_count=8
-chunk=0.5
-files=[base_path+f for f in os.listdir(base_path) if ".csv" in f and "dev_" in f and "_500_" not in f and "Haralick" not in f and "lbp." not in f]# and "deep_" not in f]
-files.sort()
-print(len(files),files)
-for f in files:
-    df=pd.read_csv(f)
-    #print(df.columns)
-    df['filename']=df['class1']+"_"+df['img']
-    
-    df=df.sort_values(by='filename')
-    print(f.split("/")[-1],df.shape,df['filename'][:5])
+from file_operations import read_files
 
 class TreeNet:
-  def __init__(self,selected_model_name,output_layer,weights,dataset,epochs,test_size,trained_weights_path,batch_size=16):
+  def __init__(self,layer_count=2,breath_count=1):
     self.layers={}
-    self.layer_count=2
-    
-    #self.densenet169=DenseNet169(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
-    #self.resnet152=ResNet152(include_top=True, weights=None, input_tensor=None, input_shape=None, pooling=None, classes=1000)
-    #self.selected_model="dense"
-    #self.output_layer='avg_pool'
-    #self.selected_model_name=selected_model_name
-    #self.output_layer=output_layer
-    self.num_classes=dataset.num_classes
-    self.epochs=epochs#5
-    self.test_size=test_size#0.33
-    self.init_weights=weights#'imagenet'
-    self.trained_weights_path=trained_weights_path#"abc.h5"
-    self.dataset=dataset
-    self.batch_size=batch_size
-
-  def read_all(files_dev):
-    train_X=pd.DataFrame()
-    train_Y=pd.DataFrame()
-
-    for f in files_dev:
-        print(f,train_X.shape)
-        if("lbp" in f):
-            df=pd.read_csv(f).iloc[:,1:]
-            df["file_name"]=df['Yl']+"__"+df['filename'].astype('str')+".jpg"
-            df.set_index("file_name",inplace=True)
-            train_Y=df['Yl']
-            df.drop(columns=['Yl','Y','filename','Unnamed: 0'],axis=1,inplace=True,errors='ignore')
-            if(train_X.shape[0]==0):
-                train_X=df
-            else:
-                train_X=train_X.merge(df,on='key',how='inner',suffixes=("","_"+file_name.split('/')[-1].split(".")[0][4:9]))
-
-        elif("densenet" in f):
-            df=pd.read_csv(f).iloc[:,1:]
-            df[['Y','img']] = df['image_name'].str.split('__',expand=True)
-            df.rename(columns={"image_name": "file_name"},inplace=True)
-            df.set_index("file_name",inplace=True)
-            
-            train_Y=df['Y']
-            df.drop(['Actual','Pred','Y','img','Unnamed: 0'],axis=1,inplace=True,errors='ignore')
-            if(train_X.shape[0]==0):
-                train_X=df
-            else:
-                train_X=train_X.merge(df,on='key',how='inner',suffixes=("","_"+file_name.split('/')[-1].split(".")[0][4:9]))
-        elif("mobilenetv2" in f):
-            df=pd.read_csv(f).iloc[:,1:-2].set_index('image_name')
-            if(train_X.shape[0]==0):
-                train_X=df
-            else:
-                train_X=train_X.merge(df,on='key',how='inner',suffixes=("","_"+file_name.split('/')[-1].split(".")[0][4:9]))
-        else:
-            df=pd.read_csv(f).iloc[:,1:]
-            df["file_name"]=df['class1']+"__"+df['img'].astype('str')
-            df.set_index("file_name",inplace=True)
-            train_Y=df['class1']
-            df.drop(['class1','img','Unnamed: 0'],axis=1,inplace=True,errors='ignore')
-            print(train_X.shape,df.shape)
-            if(train_X.shape[0]==0):
-                train_X=df
-            else:
-                train_X=train_X.merge(df,on='key',how='inner',suffixes=("","_"+file_name.split('/')[-1].split(".")[0][4:9]))
-
-    print(train_X.shape,train_Y.shape)
-    return train_X,train_Y
-
-  def design_network(self):
-    for i in range(self.layer_count):
+    self.layer_count=layer_count
+    self.breath_count=breath_count
+    for i in range(layer_count):
       self.layers["layer_"+str(i)]={}
       if(i%2==0):
-        self.layers["layer_"+str(i)]["RF_1"]=RandomForestClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        #self.layers["layer_"+str(i)]["RF_2"]=RandomForestClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        #self.layers["layer_"+str(i)]["RF_3"]=RandomForestClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        #self.layers["layer_"+str(i)]["RF_4"]=RandomForestClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        #self.layers["layer_"+str(i)]["RF_5"]=RandomForestClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        self.layers["layer_"+str(i)]["RF_6"]=RandomForestClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        self.layers["layer_"+str(i)]["ET_7"]=ExtraTreesClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        #self.layers["layer_"+str(i)]["ET_8"]=ExtraTreesClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        #self.layers["layer_"+str(i)]["ET_9"]=ExtraTreesClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        self.layers["layer_"+str(i)]["ET_10"]=ExtraTreesClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        #self.layers["layer_"+str(i)]["ET_11"]=ExtraTreesClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
-        #self.layers["layer_"+str(i)]["ET_12"]=ExtraTreesClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
+        for b in range(self.breath_count):
+          self.layers["layer_"+str(i)]["RF_1_"+str(b)]=RandomForestClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
+          self.layers["layer_"+str(i)]["RF_2_"+str(b)]=RandomForestClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
+          self.layers["layer_"+str(i)]["ET_3_"+str(b)]=ExtraTreesClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
+          self.layers["layer_"+str(i)]["ET_4_"+str(b)]=ExtraTreesClassifier(n_estimators=random.randint(1,8)*25, random_state=42)
       else:
-        #self.layers["layer_"+str(i)]["XG_1"]=XGBClassifier()
-        self.layers["layer_"+str(i)]["XG_2"]=XGBClassifier()
-        #self.layers["layer_"+str(i)]["CB_3"]=CatBoostClassifier(iterations=random.randint(1,8)*25, learning_rate=0.1, depth=6, loss_function='MultiClass', random_state=42)
-        self.layers["layer_"+str(i)]["CB_4"]=CatBoostClassifier(iterations=random.randint(1,8)*25, learning_rate=0.1, depth=6, loss_function='MultiClass', random_state=42)
-        #self.layers["layer_"+str(i)]["CB_5"]=CatBoostClassifier(iterations=random.randint(1,8)*25, learning_rate=0.1, depth=6, loss_function='MultiClass', random_state=42)
-        self.layers["layer_"+str(i)]["CB_6"]=CatBoostClassifier(iterations=random.randint(1,8)*25, learning_rate=0.1, depth=6, loss_function='MultiClass', random_state=42)
-        #self.layers["layer_"+str(i)]["CB_7"]=CatBoostClassifier(iterations=random.randint(1,8)*25, learning_rate=0.1, depth=6, loss_function='MultiClass', random_state=42)
-        #self.layers["layer_"+str(i)]["CB_8"]=CatBoostClassifier(iterations=random.randint(1,8)*25, learning_rate=0.1, depth=6, loss_function='MultiClass', random_state=42)
-        #self.layers["layer_"+str(i)]["BC_9"]=BaggingClassifier(DecisionTreeClassifier(), n_estimators=random.randint(1,4)*50, random_state=42)
-        #self.layers["layer_"+str(i)]["BC_10"]=BaggingClassifier(DecisionTreeClassifier(), n_estimators=random.randint(1,4)*50, random_state=42)
-        self.layers["layer_"+str(i)]["BC_11"]=BaggingClassifier(DecisionTreeClassifier(), n_estimators=random.randint(1,4)*50, random_state=42)
-        #self.layers["layer_"+str(i)]["BC_12"]=BaggingClassifier(DecisionTreeClassifier(), n_estimators=random.randint(1,4)*50, random_state=42)
-
-
-train_Y, uniques = pd.factorize(train_Y)
-print(train_Y)  # mapped values from 0 to 7 (or 0 to n-1)
-print(uniques)   # original values corresponding to each label
-
-
-
-  def portion(self,chunk=0.5):
-    trainX=train_X[0] #merge all
-    for i,X in enumerate(train_X[1:]):
-      trainX=trainX.merge(X,left_index=True,right_index=True,how='inner',suffixes=("","_"+files[i+1].split('/')[-1].split(".")[0][4:9]))
-    #train_Y-=1
-    shuffled_indices = np.random.permutation(len(trainX))
-    # Apply the same shuffle to both
-    trainX = trainX.iloc[shuffled_indices].reset_index(drop=True)
-    train_Y = train_Y[shuffled_indices]
-    # Select 50% of the rows
-    half_size = int(len(trainX)*chunk)
-    trainX = trainX.iloc[:half_size]
-    train_Y = train_Y[:half_size]
-    return trainX,train_Y
-
-  def fit_model(self):
-    for l,layer in enumerate(layers):
-      print("Input Shape Before Layer ",layer,"\t",trainX.shape)
-      preds={}
-      for i,forest in enumerate(layers[layer]):
-        if(l==0):
-          print("Started\n Forest\t",forest,"\t of Layer:",l+1)
-          preds[forest]=layers[layer][forest].fit(trainX, train_Y).predict_proba(trainX)
-        else:
-          print("Started\n Forest\t",forest,"\t of Layer:",l+1)
-          if("CB_" in forest):
-            #print("CB_ started")
-            preds[forest]=layers[layer][forest].fit(trainX, train_Y,verbose=0).predict_proba(trainX)
-          else:
-            preds[forest]=layers[layer][forest].fit(trainX, train_Y).predict_proba(trainX)
-      for forest in layers[layer]:
-        print(trainX.shape,)
-        trainX=trainX.merge(pd.DataFrame(preds[forest]).add_suffix("_"+layer+"_"+forest), left_index=True, right_index=True,copy=True)
-      print(len(preds))
-
-
-
-train_X=[]
-test_X=[]
-train_Y=None
-test_Y=None
-for f in files:
-    print(f.split("/")[-1])
-    df=pd.read_csv(f)
-    df['filename']=df['class1']+"_"+df['img']
-    #df['filename']=df['labels']+"_"+df['filename']
-    df=df.sort_values(by='filename')
-    train_X.append(df.drop(columns={'class','img','class1','filename'},axis=1))
-    train_Y=df["class"]-1
-    print(df.shape,len(train_X),train_X[0].shape)
-
-for f in files:
-    f=f.replace("dev_","val_")
-    print(f)
-    df=pd.read_csv(f)
-    df['filename']=df['class1']+"_"+df['img']
-    #df['filename']=df['labels']+"_"+df['filename']
-    df=df.sort_values(by='filename')
+        for b in range(self.breath_count):
+          self.layers["layer_"+str(i)]["XG_1_"+str(b)]=XGBClassifier()
+          self.layers["layer_"+str(i)]["CB_2_"+str(b)]=CatBoostClassifier(iterations=random.randint(1,8)*25, learning_rate=0.1, depth=6, loss_function='MultiClass', random_state=42)
+          self.layers["layer_"+str(i)]["CB_3_"+str(b)]=CatBoostClassifier(iterations=random.randint(1,8)*25, learning_rate=0.1, depth=6, loss_function='MultiClass', random_state=42)
+          self.layers["layer_"+str(i)]["BC_4_"+str(b)]=BaggingClassifier(DecisionTreeClassifier(), n_estimators=random.randint(1,8)*25, random_state=42)
     
-    test_X.append(df.drop(columns={'class','img','class1','filename'},axis=1))
-    #test_X.append(df.drop(columns={'labels','filename','Y'},axis=1))
-    test_Y=df["class"]-1
-    print(f.split("/")[-1],df.shape,len(test_X),test_X[0].shape)
-df=None
-#train_X=pd.read_csv(f).drop(columns={'labels','filename','Y'},axis=1)
+  def train(self,trainX,trainY):
+    for l,layer in enumerate(self.layers):
+      print("Training Layer\t",l+1,"\t of Forest\t",forest,"\t with input\t",trainX.shape)
+      preds={}
+      for i,forest in enumerate(self.layers[layer]):
+        if("CB_" in forest):
+          preds[forest]=self.layers[layer][forest].fit(trainX, trainY,verbose=0).predict_proba(trainX)
+        else:
+          preds[forest]=self.layers[layer][forest].fit(trainX, trainY).predict_proba(trainX)
+      for forest in self.layers[layer]:
+        trainX=trainX.merge(pd.DataFrame(preds[forest]).add_suffix("_"+layer+"_"+forest), left_index=True, right_index=True,copy=True)
+  def predict_prob(self,testX):
+    for l,layer in enumerate(self.layers):
+      print("Input Shape Before Layer ",layer,"\t",testX.shape)
+      preds={}
+      for i,forest in enumerate(self.layers[layer]):
+        preds[forest]=self.layers[layer][forest].predict_proba(testX)
+      for forest in self.layers[layer]:
+        testX=testX.merge(pd.DataFrame(preds[forest]).add_suffix("_"+layer+"_"+forest), left_index=True, right_index=True,copy=True)
+    return np.mean(np.stack(list(preds.values())), axis=0)
+
+    
+  def predict(self,testX):
+    for l,layer in enumerate(self.layers):
+      print("Input Shape Before Layer ",layer,"\t",testX.shape)
+      preds={}
+      for i,forest in enumerate(self.layers[layer]):
+        preds[forest]=self.layers[layer][forest].predict_proba(testX)
+      for forest in self.layers[layer]:
+        testX=testX.merge(pd.DataFrame(preds[forest]).add_suffix("_"+layer+"_"+forest), left_index=True, right_index=True,copy=True)
+    return np.argmax(np.mean(np.stack(list(preds.values())), axis=0), axis=1).reshape(-1, 1)
+
+
+
+    
+  def summary(self):
+    print("Model has"+str(self.layer_count)," layers")
+    print("Model Layer Breath is "+str(self.breath_count))
+    print("Model Details")
+    print(self.layers)
+
+treenet=TreeNet(2,3)
+treenet.summary()
