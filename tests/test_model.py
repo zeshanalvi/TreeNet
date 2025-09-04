4057f1fb5c6c7d00ef80eefd5b3b18056843f03d
zeshanalvi <zeshankhanalvi@gmail.com>
2025-09-04 09:41:36 +0500

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,7 +4,45 @@ from treenet import TreeNet
 
 def test_model_training_and_prediction():
     y=4
-    n_samples=10
+    n_samples=100
+    col_labels = [''.join(random.choices(string.ascii_uppercase, k=3)) for _ in range(y)]
+
+    X_train = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
+    X_test = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
+    y_train = np.random.randint(0, y, size=(n_samples,))
+
+
+    model = TreeNet(layer_count=3, breath_count=2)
+    model.train(X_train, y_train)
+
+    probs = model.predict_prob(X_test)
+    assert probs.shape == (n_samples, y)
+
+    preds = model.predict(X_test)
+    assert preds.shape == (n_samples,1)
+
+def test_model_dataframes():
+    y=4
+    n_samples=100
+    col_labels = [''.join(random.choices(string.ascii_uppercase, k=3)) for _ in range(y)]
+
+    X_train = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
+    X_test = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
+    y_train = np.random.randint(0, y, size=(n_samples,))
+
+
+    model = TreeNet(layer_count=3, breath_count=2)
+    model.train(X_train, y_train)
+
+    probs = model.predict_prob(X_test)
+    assert probs.shape == (n_samples, y)
+
+    preds = model.predict(X_test)
+    assert preds.shape == (n_samples,1)
+
+def test_model_ndarray():
+    y=4
+    n_samples=100
     col_labels = [''.join(random.choices(string.ascii_uppercase, k=3)) for _ in range(y)]
 
     X_train = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
