4c8eb99c53724f46cce51dfb82fa77c3b5c662d8
zeshanalvi <zeshankhanalvi@gmail.com>
2025-09-04 10:11:07 +0500

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -28,7 +28,7 @@ def test_model_dataframes():
 
     X_train = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
     X_test = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
-    y_train = np.random.randint(0, y, size=(n_samples,))
+    y_train = pd.DataFrame(np.random.randint(0, y, size=(n_samples,)))
 
 
     model = TreeNet(layer_count=3, breath_count=2)
@@ -43,10 +43,8 @@ def test_model_dataframes():
 def test_model_ndarray():
     y=4
     n_samples=100
-    col_labels = [''.join(random.choices(string.ascii_uppercase, k=3)) for _ in range(y)]
-
-    X_train = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
-    X_test = pd.DataFrame(np.random.rand(n_samples, y), columns=col_labels)
+    X_train = np.random.rand(n_samples, y)
+    X_test = np.random.rand(n_samples, y)
     y_train = np.random.randint(0, y, size=(n_samples,))
 
 
